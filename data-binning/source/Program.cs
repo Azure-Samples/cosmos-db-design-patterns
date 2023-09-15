efa1e437f536e0ea59b2c4fe46a376a455da4b14
Sandeep Nair <nair.sandeep@microsoft.com>
2023-09-15 15:59:57 +0530

    Converting to multi thread

--- data-binning/source/Program.cs
+++ data-binning/source/Program.cs
@@ -7,121 +7,91 @@ using static Azure.Core.HttpHeader;
 using Microsoft.Extensions.Configuration;
 using Container = Microsoft.Azure.Cosmos.Container;
 using Database = Microsoft.Azure.Cosmos.Database;
+using Cosmos_data_binning;
 
-namespace data_binning
+namespace DataBinning
 {
+
     internal class Program 
     {
-
-
-
-        static DateTime TruncateToMinute(DateTime time)
-        {
-            return new DateTime(time.Year, time.Month, time.Day, time.Hour, time.Minute, 0, DateTimeKind.Utc);
-        }
-
-        public static async Task SimulateEvents(int deviceCount, int timeout)
-        {
-            Database db;
-            Container container;
-            string partitionKeyPath = "/DeviceId";
-
-            var configuration = new ConfigurationBuilder()
-                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                 .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true);
-
-            var config = configuration.Build();
-
-
-            string uri = config["CosmosUri"];
-            string key = config["CosmosKey"];
-
-            CosmosClient client = new(
-                accountEndpoint: uri!,
-                authKeyOrResourceToken: key!);
-
-
-            string databaseName = "Hotels";
-            string containerName = "SensorEvents";
-
-            db = client.CreateDatabaseIfNotExistsAsync(databaseName).Result;
-            container = db.CreateContainerIfNotExistsAsync(id: containerName, partitionKeyPath: partitionKeyPath, throughput: 400).Result;
-
-            List<SensorEvent> sensorEvents = new List<SensorEvent>();
-
-            var time = DateTime.UtcNow;
-            var currentTimeMinute = TruncateToMinute(time);
-            var endTime = currentTimeMinute.AddMinutes(timeout);
-            var nextPublishTime = TruncateToMinute(time);
-
-            while (currentTimeMinute <= endTime)
-            {
-                // Sleep then increment time
-                System.Threading.Thread.Sleep(1000);
-                time = DateTime.UtcNow;
-                currentTimeMinute = TruncateToMinute(time);
-
-                // Only generate events on 5 second interval
-                if (time.Second % 5 == 0)
-                {
-                    // System.Console.WriteLine(time.Second);
-                    var events = SensorEvent.GenerateSensorEvents(deviceCount);
-                    events.ForEach(e => sensorEvents.Add(e));
-
-                    // Only publish at 1 minute interval (seconds = 00)
-                    if (currentTimeMinute > nextPublishTime)
-                    {
-                        System.Console.WriteLine($"Calculating batch for {nextPublishTime.ToString()}");
-                        var aggTimestamp = DateTime.UtcNow.ToString();
-                        var summaryEvents = sensorEvents.GroupBy(e => e.DeviceId)
-                        .Select(e => new SummarySensorEvent
-                        {
-                            DeviceId = e.Key,
-                            eventTimestamp = nextPublishTime.ToString(),
-                            numberOfReadings = e.Count(),
-                            avgTemperature = e.Average(ea => ea.Temperature),
-                            minTemperature = e.Min(ee => ee.Temperature),
-                            maxTemperature = e.Max(ee => ee.Temperature),
-                            readings = e.Select(ee => new Reading
-                            {
-                                eventTimestamp = ee.EventTimestamp,
-                                temperature = ee.Temperature
-                            }).ToArray(),
-                            receivedTimestamp = aggTimestamp
-                        }).ToList();
-
-                        summaryEvents.ForEach(row => System.Console.WriteLine($"{row.DeviceId}, {row.numberOfReadings}, {row.eventTimestamp}"));
-                        summaryEvents.ForEach(row => container.CreateItemAsync(row, new PartitionKey(row.DeviceId)).Wait());
-
-                        nextPublishTime = currentTimeMinute;
-                        sensorEvents = new List<SensorEvent>();
-                    }
-                }
-            }
-
-        }
+        private static object? _lock;
 
         static async Task Main(string[] args)
         {
-            List<SensorEvent> sensorEvents = new List<SensorEvent>();
-
             Console.WriteLine("This code will generate sensor events and bucket them by minute before saving to an Azure Cosmos DB for NoSQL account.");
+       
+            Container container = await createCosmosDBArtifactsAsync();
 
             Console.WriteLine("How many devices would you like to generate data for (enter number between 1 and 100)?");
             var deviceCountInput = Console.ReadLine();
-            
-            Console.WriteLine("How many minutes would you like to generate data for (enter number between 1 and 10)?");
-            var timeoutInput = Console.ReadLine();
-
             int.TryParse(deviceCountInput, out int deviceCount);
+
+            Console.WriteLine("How many minutes would you like to generate data for (enter number between 1 and 10)?");
+            var timeoutInput = Console.ReadLine();            
             int.TryParse(timeoutInput, out int timeout);
+
+            var currtime = System.DateTime.UtcNow;
+            var finalBatchtime = Utility.GetNextPublishTime(currtime.AddMinutes(timeout));
+            int durationSec = (int) (finalBatchtime - currtime).Duration().TotalSeconds;
 
             Console.WriteLine($"Please wait while events are simulated for {timeoutInput} minutes.");
 
-            await SimulateEvents(deviceCount, timeout);
-            
-            System.Console.WriteLine($"Function completed generation events for {deviceCountInput} devices");
-            Console.WriteLine($"Check SensorEventContainer for new sensor events");
+            _lock = new object();
+            for (int i = 0; i < deviceCount; i++)
+            {
+                WorkerThread wt = new WorkerThread(
+                    timeout,
+                    container,
+                    (i+1).ToString(),
+                     new PostMessageCallback(MessageCallback));
+
+                Task asyncTask = Task.Run(() => wt.SimulateEvents());
+            }
+
+            //wait till all threads complete.
+            await Task.Delay((durationSec +2) * 1000);
+
+            System.Console.WriteLine($"Completed generation events for {deviceCountInput} devices");
+            Console.WriteLine($"Check DataBinning Container for sensor events");
+        }
+
+        private async static Task<Container> createCosmosDBArtifactsAsync()
+        {
+            string partitionKeyPath = "/DeviceId";
+
+            Console.WriteLine($"Please wait while Cosmos DB database and container is created.");
+
+            var configuration = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                 .AddJsonFile($"appsettings.development.json", optional: true);
+
+            var config = configuration.Build();
+
+            string uri = config["CosmosUri"]!;
+            string key = config["CosmosKey"]!;
+
+            CosmosClient client = new(accountEndpoint: uri, authKeyOrResourceToken: key);
+
+            Database database = await client.CreateDatabaseIfNotExistsAsync(
+                    id: "CosmosPatterns",
+                    throughputProperties: ThroughputProperties.CreateAutoscaleThroughput(1000)
+                );
+
+            Container container = await database.CreateContainerIfNotExistsAsync(
+                    id: "DataBinning",
+                    partitionKeyPath: partitionKeyPath
+                ); 
+
+            return container;
+
+        }
+
+        private static void MessageCallback(string message)
+        {
+            lock (_lock!)
+            {
+                Console.WriteLine(message);
+            }
         }
     }
 }
---
+++ data-binning/source/WorkerThread.cs
@@ -0,0 +1,89 @@
+﻿using DataBinning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Serialization.HybridRow.RecordIO;
+
+namespace Cosmos_data_binning
+{
+    // Delegate that defines the signature for the callback method.
+    public delegate void PostMessageCallback(string msg);
+
+    internal class WorkerThread
+    {
+        int timeout;
+        Container container;
+        string deviceId;
+        PostMessageCallback postMessage;
+
+        public WorkerThread(int timeout, Container container, string deviceId, PostMessageCallback postMessage) 
+        {
+            this.timeout = timeout;
+            this.container = container;
+            this.deviceId = deviceId;
+            this.postMessage = postMessage;
+        }
+                
+
+        public async void SimulateEvents()
+        {
+            List<SensorEvent> sensorEvents = new List<SensorEvent>();
+
+            var time = DateTime.UtcNow;
+
+            var endTime = time.AddMinutes(timeout);
+            var nextPublishTime = Utility.GetNextPublishTime(time);
+            postMessage($"Device#{deviceId}: Current Time is {time}, next batch publishing at {nextPublishTime.ToString()}");
+            
+            while (true)
+            {
+                // Sleep then increment time,  
+                System.Threading.Thread.Sleep(1000);
+                time = DateTime.UtcNow;
+
+                //Only generate events when seconds is a multiple of 5
+                if (time.Second % 5 == 0)
+                {
+
+                    var sensorEvent = SensorEvent.GenerateSensorEvent(deviceId);
+                    sensorEvents.Add(sensorEvent);
+
+                    // Only publish at 1 minute interval (seconds = 00)
+                    if (time > nextPublishTime)
+                    {
+                        SummarySensorEvent eventSummary=new SummarySensorEvent
+                        {
+                            DeviceId = deviceId,
+                            eventTimestamp = nextPublishTime.ToString(),
+                            numberOfReadings = sensorEvents.Count(),
+                            avgTemperature = sensorEvents.Average(ea => ea.Temperature),
+                            minTemperature = sensorEvents.Min(ee => ee.Temperature),
+                            maxTemperature = sensorEvents.Max(ee => ee.Temperature),
+                            readings = sensorEvents.Select(ee => new Reading
+                            {
+                                eventTimestamp = ee.EventTimestamp,
+                                temperature = ee.Temperature
+                            }).ToArray(),
+                            receivedTimestamp = DateTime.UtcNow.ToString()
+                        };                                                
+
+                        await container.CreateItemAsync(eventSummary, new PartitionKey(eventSummary.DeviceId));
+
+                        nextPublishTime = Utility.GetNextPublishTime(time);
+                        postMessage($"Device#{deviceId}: Current Time is {time}, next batch publishing at {nextPublishTime.ToString()}");
+
+
+                        sensorEvents = new List<SensorEvent>();
+
+                        if (time > endTime)
+                            return;
+                    };
+                }
+            }
+        
+        }
+    }
+}
